c2d6bf833f3bf1a28584cab6a0356e71eeffa6d4
rampler <rampler@gmail.com>
2025-04-08 19:41:28 +0200

    update climate.py

--- extalife/climate.py
+++ extalife/climate.py
@@ -160,7 +160,8 @@ class ExtaLifeClimate(ExtaLifeChannel, ClimateEntity):
             {
                 "waiting_to_synchronize": data.get("waiting_to_synchronize"),
                 "temperature_old": data.get("temperature_old"),
-                "battery_status": data.get("battery_status")
+                "battery_status": data.get("battery_status"),
+                "last_sync": data.get("last_sync")
             }
         )
 
