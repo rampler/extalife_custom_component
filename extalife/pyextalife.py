6cd54ef9f72803d4edb52ede5b9d6e6037cd0a0a
rampler <rampler@gmail.com>
2025-04-09 12:28:05 +0200

    update pyextalife.py

--- extalife/pyextalife.py
+++ extalife/pyextalife.py
@@ -499,7 +499,7 @@ class ExtaLifeAPI:
             return None
             
     async def get_channel_conf(self, sensor_id, channel):
-        if not isinstance(sensor_id, int) or not isinstance(channel, int):
+        if sensor_id.isnumeric() or channel.isnumeric():
           return {}
         try:
             cmd = self.CMD_FETCH_CONFIG
