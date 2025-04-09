d81938d37b8821c6ea5a7a49d22f89fbf0b72e85
rampler <rampler@gmail.com>
2025-04-09 10:30:45 +0200

    update pyextalife.py

--- extalife/pyextalife.py
+++ extalife/pyextalife.py
@@ -502,8 +502,8 @@ class ExtaLifeAPI:
         try:
             cmd = self.CMD_FETCH_CONFIG
             cmd_data = {
-             "id": sensor_id,
-             "channel": channel
+             "id": int(sensor_id),
+             "channel": int(channel)
             }
             resp = await self._connection.async_execute_command(cmd, cmd_data)
             return resp
